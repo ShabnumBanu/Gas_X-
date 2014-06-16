299bcc3af0fdc2ba0d9621b457f8b7696f7ce484
unknown <M1023696@A8MD21506.mindtree.com>
2014-06-16 15:09:03 +0530

    Added Initial Code base

---
+++ js/style.js
@@ -0,0 +1,92 @@
+
+body {
+}
+h1, h2, h3, h4 {
+    font-weight: 200;
+    letter-spacing: -1px;
+    line-height: 1;
+    margin: 20px 0;
+}
+p {
+    font-size: 12px;
+}
+.footer {
+    background-image: url("img/footer_background.gif");
+    height: 135px;
+}
+.line {
+    color: #FF0000;
+    font-size: 45px;
+    font-style: italic;
+    font-weight: bold;
+}
+.line span {
+    background-color: #FFFFFF;
+    padding: 0 20px;
+}
+.line_faq {
+    color: #FF0000;
+    font-size: 45px;
+    font-style: italic;
+    font-weight: bold;
+    text-align: left;
+}
+.line_faq span {
+    background-color: #FFFFFF;
+    padding: 0 20px;
+}
+.footer_bottom {
+    text-align: center;
+}
+.nav_shift {
+    padding-left: 50%;
+}
+.box-main {
+    background-color: #0000FF;
+    text-align: center;
+    width: 180px;
+}
+.bread_crumb {
+    color: #000000;
+    font-size: 11px;
+    margin-left: 80px;
+}
+.about_p {
+    padding-top: 20px;
+}
+.faq_heading {
+    color: #0000FF;
+}
+.details_btn {
+    float: left;
+    margin-right: 20px;
+    margin-top: 50px;
+    text-align: left;
+}
+.heading {
+    color: #0000FF;
+    float: left;
+    margin-left: 20px;
+    text-align: left;
+}
+.hr {
+    background-color: #428BCA;
+    color: #428BCA;
+    height: 5px;
+    size: 10px;
+    width: 35px;
+}
+.footer1 {
+    background-color: #808080;
+    color: #FFFFFF;
+}
+.footer2 {
+    background-image: url("../img/footer_background.png");
+    color: #FFFFFF;
+    text-align: center;
+}
+.hr2 {
+    background-color: #D8D8D8;
+    color: #D8D8D8;
+    height: 20px;
+}
